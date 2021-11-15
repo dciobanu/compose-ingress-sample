92b334bd2c4a15edbd47d557350cc8e0a9ca1890
Dorin Ciobanu <dorin@ciobanu.org>
2021-11-15 13:18:23 -0800

    Simple demo of ingressing with nginx

---
+++ app-3/src/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const { exec } = require("child_process");
+
+let netInfo = '';
+
+exec("ifconfig", (error, stdout, stderr) => {
+    netInfo = stdout;
+});
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('APP-3\n\n' + netInfo);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
